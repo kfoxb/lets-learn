00ff110f8e93070f769de4d923102e781bd3666f
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-10 18:49:45 -0800

    Fix serving static public directory, add a static asset directory to server. Add /api/img get route

--- Server/server-config.js
+++ Server/server-config.js
@@ -1,10 +1,20 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let morgan = require('morgan');
+let mongoose = require('mongoose');
+let Images = require('../app/models/img');
+let db = require('../db/config.js');
 
 let app = express();
 
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/../assets'));
+console.log(__dirname);
 app.use(morgan('dev'));
+app.get('/api/img', (req, res) => {
+  Images.find().then((images) => { 
+    res.json(images); 
+  });
+});
 
 module.exports = app;
