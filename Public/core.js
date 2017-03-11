15241e0083267cc337d702e13ac62b9aae653f9c
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-10 19:46:49 -0800

    Create letsLearnColors module and controller in core.js

--- Public/core.js
+++ Public/core.js
@@ -1,1 +1,17 @@
 //this file will contain logic for angular
+let letsLearnColors = angular.module('letsLearnColors', []);
+
+function mainController($scope, $http) {
+  $scope.formData = {};
+
+  // when landing on the page, get all todos and show them
+  $http.get('/api/img')
+    .success(function(data) {
+      $scope.images = data;
+      console.log(data);
+    })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
+
+}
