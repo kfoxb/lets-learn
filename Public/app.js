42da258b4e60c21721c8a61b15b38462ce723339
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-11 15:19:46 -0800

    implement individual sounds per image in angular

--- Public/app.js
+++ Public/app.js
@@ -1,16 +1,47 @@
 let letsLearnColors = angular.module('letsLearnColors', [])
-
-.controller('appController', ['$scope', '$http', function($scope, $http) {
-  $scope.playAudio = function() {
-    var audio = new Audio('aud/orange.wav');
-    console.log(audio);
+.directive('app', function() {
+  return {
+    restrict: 'E',
+    controller: function($scope, $http) {
+      $http.get('/api/items')
+        .success(function(data) {
+          $scope.items = data;
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    // $scope.items = [0, 1]//http get here;
+    },
+    template: '<items-container></items-container>',
+  };
+})
+.directive('itemsContainer', function() {
+  return {
+    restrict: 'E',
+    template: `
+      <div>
+        <item ng-repeat="item in items" item="item"></item>
+      </div>`
+  };
+})
+.directive('item', function() {
+  return {
+    controller: 'itemController',
+    restrict: 'E',
+    replace: true,
+    template: `
+      <div>
+        <span class="item-name">{{item.title}}</span>
+        <span>
+          <img ng-src={{item.imgUrl}} ng-click="playAudio()"/>
+        </span>
+      </div>`
+  };
+})
+.controller('itemController', function($scope) {
+  let audUrl = $scope.item.audUrl;
+  $scope.playAudio = function($scope) {
+    var audio = new Audio(audUrl);
     audio.play();
   };
-  $http.get('/api/items')
-    .success(function(data) {
-      $scope.images = data;
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
-    });
-}]);
+});
