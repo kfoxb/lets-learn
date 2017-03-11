c3a225203f7914372be69ab4a26b9bb20fca7e7a
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-10 17:32:26 -0800

    make mongoose use bluebird for promises

--- db/config.js
+++ db/config.js
@@ -1,4 +1,5 @@
 //this file will be used for database configuration
 var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
 mongoose.connect('mongodb://localhost/letslearncolors');
