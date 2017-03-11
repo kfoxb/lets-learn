4858d3c441645e0b78819de2afb7256cdb1b95f8
Kyle Bradford <kfoxb@kfoxb.com>
2017-03-10 16:10:30 -0800

    Add require mongoose to img model and export model as Images

--- app/models/img.js
+++ app/models/img.js
@@ -1,4 +1,8 @@
-var Todo = mongoose.model('images', {
+let mongoose = require('mongoose');
+
+let Images = mongoose.model('Images', {
   title: String,
   url: String
 });
+
+module.exports = Images;
